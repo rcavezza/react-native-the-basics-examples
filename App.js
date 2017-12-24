8d1a4291068dc9efa210f1e5d310fb1533b01ae3
rcavezza <rcavezza@gmail.com>
2017-12-24 12:40:26 -0500

    Align Item Basics

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 
-export default class JustifyContentBasics extends Component {
+export default class AlignItemsBasics extends Component {
   render() {
     return (
-      // Try setting `justifyContent` to `center`.
+      // Try setting `alignItems` to 'flex-start'
+      // Try setting `justifyContent` to `flex-end`.
       // Try setting `flexDirection` to `row`.
       <View style={{
         flex: 1,
         flexDirection: 'column',
-        justifyContent: 'space-between',
+        justifyContent: 'center',
+        alignItems: 'center',
       }}>
         <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
         <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
