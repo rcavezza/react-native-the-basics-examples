3b6c15cc283e5fc79fd45230384cb6faff121d45
rcavezza <rcavezza@gmail.com>
2017-12-24 12:48:36 -0500

    Pizza Translator showing off textInput and state

--- App.js
+++ App.js
@@ -1,22 +1,27 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { Text, TextInput, View } from 'react-native';
 
-export default class AlignItemsBasics extends Component {
+export default class PizzaTranslator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: '' };
+  }
+
   render() {
     return (
-      // Try setting `alignItems` to 'flex-start'
-      // Try setting `justifyContent` to `flex-end`.
-      // Try setting `flexDirection` to `row`.
-      <View style={{
-        flex: 1,
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-        <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
-        <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+      <View style={{padding: 10, marginTop: 100}}>
+        <TextInput
+          style={{height: 40}}
+          placeholder="Type here to translate!"
+          onChangeText={(text) => this.setState({text})}
+        />
+        <Text style={{padding: 10, fontSize: 42}}>
+          {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
+        </Text>
+        <Text style={{padding: 10, fontSize: 20}}>
+          {this.state.text}
+        </Text>
       </View>
     );
   }
-};
+}
