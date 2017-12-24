473a8344d7a085818edde1f630d304b1a430cb78
rcavezza <rcavezza@gmail.com>
2017-12-24 13:28:38 -0500

    FlatList basics

--- App.js
+++ App.js
@@ -1,42 +1,71 @@
 import React, { Component } from 'react';
-import { ScrollView, Image, Text } from 'react-native';
+import { AppRegistry, FlatList, StyleSheet, Text, View } from 'react-native';
 
-export default class IScrolledDownAndWhatHappenedNextShockedMe extends Component {
+export default class FlatListBasics extends Component {
   render() {
-      return (
-        <ScrollView>
-          <Text style={{fontSize:96}}>Scroll me plz</Text>
-          <Image source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg' }} style={{width: 193, height: 110}} />
-          <Image source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg' }} style={{width: 193, height: 110}} />
-          <Image source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg' }} style={{width: 193, height: 110}} />
-          <Image source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg' }} style={{width: 193, height: 110}} />
-          <Image source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg' }} style={{width: 193, height: 110}} />
-          <Text style={{fontSize:96}}>If you like</Text>
-          <Image source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg' }} style={{width: 193, height: 110}} />
-          <Image source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg' }} style={{width: 193, height: 110}} />
-          <Image source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg' }} style={{width: 193, height: 110}} />
-          <Image source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg' }} style={{width: 193, height: 110}} />
-          <Image source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg' }} style={{width: 193, height: 110}} />
-          <Text style={{fontSize:96}}>Scrolling down</Text>
-          <Image source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg' }} style={{width: 193, height: 110}} />
-          <Image source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg' }} style={{width: 193, height: 110}} />
-          <Image source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg' }} style={{width: 193, height: 110}} />
-          <Image source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg' }} style={{width: 193, height: 110}} />
-          <Image source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg' }} style={{width: 193, height: 110}} />
-          <Text style={{fontSize:96}}>What's the best</Text>
-          <Image source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg' }} style={{width: 193, height: 110}} />
-          <Image source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg' }} style={{width: 193, height: 110}} />
-          <Image source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg' }} style={{width: 193, height: 110}} />
-          <Image source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg' }} style={{width: 193, height: 110}} />
-          <Image source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg' }} style={{width: 193, height: 110}} />
-          <Text style={{fontSize:96}}>Framework around?</Text>
-          <Image source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg' }} style={{width: 193, height: 110}} />
-          <Image source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg' }} style={{width: 193, height: 110}} />
-          <Image source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg' }} style={{width: 193, height: 110}} />
-          <Image source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg' }} style={{width: 193, height: 110}} />
-          <Image source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg' }} style={{width: 193, height: 110}} />
-          <Text style={{fontSize:80}}>React Nativez</Text>
-        </ScrollView>
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={[
+            {key: 'Devin'},
+            {key: 'Jackson'},
+            {key: 'James'},
+            {key: 'Joel'},
+            {key: 'Jommmmmmhn'},
+            {key: '1Jillian'},
+            {key: '2Jimmy'},
+            {key: '3Julie'},
+            {key: '5Joel'},
+            {key: '6John'},
+            {key: '7Jillian'},
+            {key: '9Jimmy'},
+            {key: '8Julie'},
+            {key: 'J23oel'},
+            {key: 'Joh3n'},
+            {key: 'Jil5lian'},
+            {key: 'Jim5my'},
+            {key: 'Ju3lie'},
+            {key: 'Joael'},
+            {key: 'Jobbbhn'},
+            {key: 'Ji1llian'},
+            {key: 'Jimmy'},
+            {key: 'Julbie'},
+            {key: 'Joecl'},
+            {key: 'Jozhn'},
+            {key: 'Jidllian'},
+            {key: 'Jimcmy'},
+            {key: 'Julzie'},
+            {key: 'Jocel'},
+            {key: 'h23n'},
+            {key: 'Jil53235lian'},
+            {key: 'Jimddmy'},
+            {key: 'Jccculie'},
+            {key: 'Joccel'},
+            {key: 'Jcccon'},
+            {key: 'Jicccllian'},
+            {key: 'Jimbo Fisher'},
+            {key: 'Jubbbblie'},
+            {key: 'Jo333el'},
+            {key: 'Jofghhhhn'},
+
+          ]}
+          renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+        />
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   paddingTop: 22
+  },
+  item: {
+    backgroundColor: 'blue',
+    color: 'red',
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+})
